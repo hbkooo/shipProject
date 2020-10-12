db22cfded89d979ebbc6ce9392b5cbfad3c60eaa
BingkaiHan <32959021+hbkooo@users.noreply.github.com>
2020-10-12 19:05:44 +0800

    Update ProjectCaffeShip.cmake

--- cmake/ProjectCaffeShip.cmake
+++ cmake/ProjectCaffeShip.cmake
@@ -3,9 +3,9 @@ find_library(CAFFE_LIBRARY caffe ${CMAKE_SOURCE_DIR}/deps/caffe_ship/caffe_fast_
 set(CAFFE_LIBRARY ${CMAKE_SOURCE_DIR}/deps/caffe_ship/caffe_fast_rcnn/build/lib/libcaffe.so)
 set(CAFFE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/caffe_ship/caffe_fast_rcnn/include)
 
-# python path
-#set(PYTHON_LIBRARY /home/hbk/miniconda3/envs/python2/lib/libpython2.7.so)
-#set(PYTHON_INCLUDE_DIR /home/hbk/miniconda3/envs/python2/include/python2.7)
+# python path，修改为你个人的python环境的路径
+set(PYTHON_LIBRARY /home/hbk/miniconda3/envs/python2/lib/libpython2.7.so)
+set(PYTHON_INCLUDE_DIR /home/hbk/miniconda3/envs/python2/include/python2.7)
 
 # user-defined python layers and caffe python module
 set(PYTHONPATH ${CMAKE_SOURCE_DIR}/deps/caffe_ship/lib ${CMAKE_SOURCE_DIR}/deps/caffe_ship/caffe_fast_rcnn/python)
@@ -19,8 +19,8 @@ set(ROTATION_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/caffe_ship/lib/rotation )
 
 message(STATUS "CAFFE_LIBRARY ${CAFFE_LIBRARY}")
 message(STATUS "CAFFE_INCLUDE_DIR ${CAFFE_INCLUDE_DIR}")
-#message(STATUS "PYTHON_LIBRARY ${PYTHON_LIBRARY}")
-#message(STATUS "PYTHON_INCLUDE_DIR ${PYTHON_INCLUDE_DIR}")
+message(STATUS "PYTHON_LIBRARY ${PYTHON_LIBRARY}")
+message(STATUS "PYTHON_INCLUDE_DIR ${PYTHON_INCLUDE_DIR}")
 message(STATUS "PYTHONPATH ${PYTHONPATH}")
 message(STATUS "ROTATION_LIBRARY ${ROTATION_LIBRARY}")
 message(STATUS "ROTATION_INCLUDE_DIR ${ROTATION_INCLUDE_DIR}")
